6bced901eef7b2b52762bb218948d753bdf1c167
Arpan Adhikari <121885175+DaemonArpan@users.noreply.github.com>
2023-02-24 21:31:12 +0545

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	//to fiind volume of cube
+
+float length;
+printf("length of the cube is =");
+scanf("%f",&length);
+printf("Volume of given cube is: %f",length*length*length );
+return 0;}
+	
+	
+	
+	
+	
+	
+	
+
